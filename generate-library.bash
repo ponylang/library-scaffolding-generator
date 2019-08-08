9eb8983d50ebcfe8c5560e1ef42b74ec96044871
Sean T. Allen <sean@monkeysnatchbanana.com>
2019-08-08 13:56:48 -0400

    Config

--- generate-library.bash
+++ generate-library.bash
@@ -3,39 +3,15 @@
 set -o errexit
 set -o nounset
 
-### Variables to replace
-
-# GitHub user or organization that owns the project
-# https://github.com/OWNER/project
-# Unless this library is part of an organization, this should be your username.g
-REPO_OWNER="ponylang"
-# Name of the project
-# https://github.com/ponylang/NAME
-REPO_NAME="crypto"
-#
-PACKAGE="crypto"
-#
-PROJECT_DESCRIPTION="Library of common cryptographic algorithms and functions for Pony."
-#
-PROJECT_STATUS="${REPO_NAME} is pre-alpha software."
-# GitHub user to access GitHub API as. Will need to provide a
-# personal access token that allows read/write access to the repository.
-# Unless this library is part of an organization, this should be your username.
-GITHUB_USER="ponylang-main"
-# Email address to associate with commits made by GITHUB_USER
-# Unless this library is part of an organization, this should be your email.
-COMMIT_EMAIL="ponylang.main@example.com"
-# Name to associate with commits made by GITHUB_USER
-# Unless this library is part of an organization, this should be your name.
-COMMIT_NAME="Main"
-# Copyright year for the License
-COPYRIGHT_YEAR="2019"
-# Copyright holder for the License
-COPYRIGHT_HOLDER="The Pony Developers"
-# email address that users should use to report code of conduct violations
-COC_EMAIL="coc@example.com"
+# Bring in user specific configuration
+source config.bash
 
 ## TODO: verify $1
+if [ $# != 1 ]
+then
+  echo "usage: generated-library.bash TARGET"
+  exit 1
+fi
 
 TARGET_DIR=$1
 
